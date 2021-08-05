a4676b8e4d3dcfed5d09a80171d3f6e03cf6f580
aRbhaSun20 <remo78295279@gmail.com>
2021-08-05 19:09:34 +0530

    update

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,28 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./style/style.css";
 
 import ImageContainer from "../../Components/ImageContainer_beforeLogin/ImageContainer";
+import { Button } from "@material-ui/core";
 
 const Home = () => {
-	let arr = [10, 9, 8, 7, 8, 9, 10, 9, 8, 7];
-	let arr2 = [10, 9, 8, 7, 8];
+	let arr = [
+		{ value: 10, title: "title1" },
+		{ value: 9, title: "title2" },
+		{ value: 8, title: "title3" },
+		{ value: 7, title: "title4" },
+		{ value: 8, title: "title5" },
+		{ value: 9, title: "title6" },
+		{ value: 10, title: "title7" },
+		{ value: 9, title: "title8" },
+		{ value: 8, title: "title9" },
+		{ value: 7, title: "title0" },
+	];
+	const [title, setTitle] = useState("");
 	return (
 		<React.Fragment>
 			<div className="container">
 				{arr.map((ele, i) => (
-					<div
+					<Button
 						key={i}
 						style={{
 							position: "relative",
-							bottom: `${ele}em`,
+							bottom: `${ele.value}em`,
+						}}
+						onMouseEnter={() => {
+							setTitle(ele.title);
 						}}
 					>
 						<ImageContainer />
-					</div>
+					</Button>
 				))}
 			</div>
-			<div className="title"></div>
+			<div className="title">{title}</div>
 		</React.Fragment>
 	);
 };

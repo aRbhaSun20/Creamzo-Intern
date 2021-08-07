0d47aa99dc39db4d55900a5a1c9867d91f3f2b6e
Rohit Nair <rohitict1@gmail.com>
2021-08-07 13:50:41 +0530

    Tnc and Board page

---
+++ src/Components/Tnc/Tnc.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Menu from "./Menu/Menu";
+import "./tnc.css";
+
+
+function Tnc() {
+	return (
+		<React.Fragment>
+			<div className="Tnc">
+				<Menu /> 
+			</div>
+		</React.Fragment>
+	);
+}
+
+export default Tnc;

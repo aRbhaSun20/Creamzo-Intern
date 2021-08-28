1e4a657a7f266558e28af028a8b9678c79768dd3
sarangyogi <sarangyogi2001@gmail.com>
2021-08-29 00:02:35 +0530

    commit for Discover and creative page

--- src/Components/Creative/Glider.jsx
+++ src/Components/Creative/Glider.jsx
@@ -1,11 +1,83 @@
 import React from "react";
 import style from './style/Creativestyle.module.css'
-import i from './assets/img1.webp'
+// import { Slide } from "react-slideshow-image";
+import Carousel from "./Carousel.jsx";
+
+function importAll(r){
+    return r.keys().map(r);
+}
+const images=importAll(require.context('./assets/',false,/\.(png|jpe?g|svg|jpg)$/));
+
+
+// const style
 const Glider = () => {
-	return (
+	console.log(images[1]);
+    // images.map(image => {
+    //     <div>
+    //         <div style={{padding: 8}}>
+    //             <img src={image} alt="placeholder" style={{width: '100%'}} />
+    //         </div>
+    //     </div>
+    // })
+	return(
 		<React.Fragment>
-			<div className={style.CreativeCarousalContainer}>
-				<div >
+			<Carousel show={3}>
+                {
+                    images.map(image =>
+                        <div>
+                            <div style={{padding: 8}} className={style.imageContainer}>
+                                <img src={image.default} alt="placeholder" style={{width: '100%',height:"370px"}} className={style.creativeImage} />
+                                <div className={style.creativeName}>
+                                    Name
+                                </div>
+                            </div>
+                            
+                        </div>
+                    )
+                }
+                {/*<div>
+                    <div style={{padding: 8}}>
+                        <img src={i} alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src={i} alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src={i} alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src={i} alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src={i} alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src={i} alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src={i} alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>
+                <div>
+                    <div style={{padding: 8}}>
+                        <img src={i} alt="placeholder" style={{width: '100%'}} />
+                    </div>
+                </div>*/}
+            </Carousel>
+			{/*<div className={style.CreativeCarousalContainer}>
+				<div>
 
 					<div className={style.imageCOrousalCOnatiner1} style={{ display: "flex" }} >
 
@@ -16,11 +88,10 @@ const Glider = () => {
 						<img className={style.corousalImageCreative} src={i} alt="" />
 						<img className={style.corousalImageCreative} src={i} alt="" />
 
-
 					</div>
 					
 				</div>
-			</div>
+			</div>*/}
 		</React.Fragment>
 	);
 };

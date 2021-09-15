145e1c46ec3a5fe5dfc97ed66f505a953b2dae80
aRbhaSun20 <remo78295279@gmail.com>
2021-09-15 20:48:59 +0530

    errors resolved

--- src/Components/Board/BoardCreation.jsx
+++ src/Components/Board/BoardCreation.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect } from "react";
+import React, { useState} from "react";
 import TextField from "@material-ui/core/TextField";
 // import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from "@material-ui/core/MenuItem";
@@ -11,6 +11,9 @@ import dots from "./assets/dots.svg";
 
 import upload from "./assets/6578872_arrow_arrows_circle_direction_up_icon (1).png";
 import { axiosSendRequest, AXIOS_ACTIONS } from "../../utils/AxiosSendRequest";
+import { usePin } from "../../Context/PinsContext";
+
+import { useSnackbar } from "notistack";
 const BoardCreation = () => {
 	const [dis, setdis] = useState("none");
 	const [display, setDisplay] = useState({
@@ -21,36 +24,37 @@ const BoardCreation = () => {
 		imgPath: "",
 		category: "new",
 	});
+	const { enqueueSnackbar } = useSnackbar();
+	
+	// eslint-disable-next-line
+	const [pinsData, refetch] = usePin();
 
 	const handlechange = (e) => {
 		console.log(e.target.name);
 		if (e.target.name === "imgPath") {
-			let dataImg = e.target.value.split(`\\`);
-			let blob = URL.createObjectURL(e.target.files[0]);
-			console.log(e.target.files[0]);
 			setDisplay((state) => ({
 				...state,
-				// [e.target.name]: `./${e.target.files[0].name}`,
-				[e.target.name]:blob,
+				[e.target.name]: e.target.files[0],
 			}));
 		} else {
 			setDisplay((state) => ({ ...state, [e.target.name]: e.target.value }));
 		}
-	  
 	};
 
-	const handledisplay=()=>{
-		if(dis==="none"){
+	const handledisplay = () => {
+		if (dis === "none") {
 			setdis("block");
-		}
-		else{
+		} else {
 			setdis("none");
 		}
-	}
+	};
 	const handleSubmit = (e) => {
 		console.log("done");
 		axiosSendRequest(AXIOS_ACTIONS.CREATE_PIN, "createPin", display).then(
-			(data) => console.log(data)
+			(data) => {
+				refetch();
+				enqueueSnackbar("Pins Created", { variant: "success" });
+			}
 		);
 	};
 
@@ -112,11 +116,11 @@ const BoardCreation = () => {
 																accept="image/*"
 																onChange={handlechange}
 															/>
-														) : (false)}
+														) : (
+															false
+														)}
 													</form>
-
 												</div>
-
 											</label>
 
 											<p
@@ -135,7 +139,14 @@ const BoardCreation = () => {
 									</div>
 								</div>
 							</div>
-							{display.imgPath && <img className={style.leftContainer} src={display.imgPath} style={{position: "relative",top: "-50%",width: "100%"}} alt=""/>}
+							{display.imgPath && (
+								<img
+									className={style.leftContainer}
+									src={URL.createObjectURL(display.imgPath)}
+									style={{ position: "relative", top: "-50%", width: "100%" }}
+									alt=""
+								/>
+							)}
 						</div>
 						<div className={style.rightContiner}>
 							<form className={style.BoardCreationForm} action="">
@@ -151,7 +162,7 @@ const BoardCreation = () => {
 										style={{
 											width: "150px",
 											height: "40px",
-											scrollBehaviour:"auto",
+											scrollBehaviour: "auto",
 											borderRadius: "10px 0px 0px 10px",
 											position: "relative",
 											backgroundColor: "rgb(230, 233, 233)",
@@ -160,18 +171,18 @@ const BoardCreation = () => {
 										disableUnderline
 										id="demo-simple-select-outlined"
 									>
-										<div style={{height:"100px",scrollBehaviour:"auto"}}>
-										<MenuItem value="section1">Section 1</MenuItem>
-										<MenuItem value="section2">Section 2</MenuItem>
-										<MenuItem value="section3">Section 3</MenuItem>
-										<MenuItem value="section4">Section 4</MenuItem>
-										<MenuItem value="section5">Section 5</MenuItem>
-										<MenuItem value="section6">Section 6</MenuItem>
-										<MenuItem value="section7">Section 7</MenuItem>
-										<MenuItem value="section8">Section 8</MenuItem>
-										<MenuItem value="section9">Section 9</MenuItem>
-										<MenuItem value="section10">Section 10</MenuItem>
-										<MenuItem value="section11">Section 11</MenuItem>
+										<div style={{ height: "100px", scrollBehaviour: "auto" }}>
+											<MenuItem value="section1">Section 1</MenuItem>
+											<MenuItem value="section2">Section 2</MenuItem>
+											<MenuItem value="section3">Section 3</MenuItem>
+											<MenuItem value="section4">Section 4</MenuItem>
+											<MenuItem value="section5">Section 5</MenuItem>
+											<MenuItem value="section6">Section 6</MenuItem>
+											<MenuItem value="section7">Section 7</MenuItem>
+											<MenuItem value="section8">Section 8</MenuItem>
+											<MenuItem value="section9">Section 9</MenuItem>
+											<MenuItem value="section10">Section 10</MenuItem>
+											<MenuItem value="section11">Section 11</MenuItem>
 										</div>
 									</Select>
 									<Button
@@ -222,6 +233,6 @@ const BoardCreation = () => {
 			</div>
 		</React.Fragment>
 	);
-}
+};
 
 export default BoardCreation;

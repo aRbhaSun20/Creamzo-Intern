4fcb5285da56245c93d10ee8d66db43140a9485a
Jayesh Jayanandan <67648301+Jayeshhunter@users.noreply.github.com>
2021-09-20 22:06:59 +0530

    update:description,

--- src/Components/Board/BoardCreation.jsx
+++ src/Components/Board/BoardCreation.jsx
@@ -4,6 +4,7 @@ import TextField from "@material-ui/core/TextField";
 import MenuItem from "@material-ui/core/MenuItem";
 // import FormHelperText from '@material-ui/core/FormHelperText';
 // import FormControl from '@material-ui/core/FormControl';
+import { useHistory } from "react-router-dom";
 import Select from "@material-ui/core/Select";
 import { Button } from "@material-ui/core";
 import style from "./style/BoardCreation.module.css";
@@ -15,253 +16,258 @@ import { usePin } from "../../Context/PinsContext";
 
 import { useSnackbar } from "notistack";
 const BoardCreation = () => {
-	const [dis, setdis] = useState("none");
-	const [display, setDisplay] = useState({
-		title: "",
-		description: "",
-		tags: "",
-		link: "",
-		imgPath: "",
-		category: "new",
-	});
-	const { enqueueSnackbar } = useSnackbar();
-	const user = JSON.parse(sessionStorage.getItem("creamzoUser"));
-	// eslint-disable-next-line
-	const [pinsData, refetch] = usePin();
+  const history = useHistory();
+  const [dis, setdis] = useState("none");
+  const [display, setDisplay] = useState({
+    title: "",
+    description: "",
+    tags: "",
+    link: "",
+    imgPath: "",
+    category: "",
+  });
+  const { enqueueSnackbar } = useSnackbar();
+  const user = JSON.parse(sessionStorage.getItem("creamzoUser"));
+  // eslint-disable-next-line
+  const [pinsData, refetch] = usePin();
 
-	const handlechange = (e) => {
-		// console.log(e.target.name);
-		// if (e.target.name === "imgPath") {
-		// 	setDisplay((state) => ({
-		// 		...state,
-		// 		[e.target.name]: e.target.files[0],
-		// 	}));
-		// } else {
-		// 	setDisplay((state) => ({ ...state, [e.target.name]: e.target.value }));
-		// }
-		if (e.target.name === "imgPath") {
-			// let dataImg = e.target.value.split(`\\`);
-			// let blob = URL.createObjectURL(e.target.files[0]);
-			// console.log(e.target.files[0]);
-			setDisplay((state) => ({
-				...state,
-				// [e.target.name]: `./${e.target.files[0].name}`,
-				[e.target.name]: e.target.files[0],
-			}));
-		} else {
-			setDisplay((state) => ({ ...state, [e.target.name]: e.target.value }));
-		}
-	};
+  const handlechange = (e) => {
+    // console.log(e.target.name);
+    // if (e.target.name === "imgPath") {
+    // 	setDisplay((state) => ({
+    // 		...state,
+    // 		[e.target.name]: e.target.files[0],
+    // 	}));
+    // } else {
+    // 	setDisplay((state) => ({ ...state, [e.target.name]: e.target.value }));
+    // }
+    if (e.target.name === "imgPath") {
+      // let dataImg = e.target.value.split(`\\`);
+      // let blob = URL.createObjectURL(e.target.files[0]);
+      // console.log(e.target.files[0]);
+      setDisplay((state) => ({
+        ...state,
+        // [e.target.name]: `./${e.target.files[0].name}`,
+        [e.target.name]: e.target.files[0],
+      }));
+    } else {
+      setDisplay((state) => ({ ...state, [e.target.name]: e.target.value }));
+    }
+  };
 
-	const handledisplay = () => {
-		if (dis === "none") {
-			setdis("block");
-		} else {
-			setdis("none");
-		}
-	};
-	const handleSubmit = (e) => {
-		console.log("done");
-		axiosSendRequest(AXIOS_ACTIONS.CREATE_PIN, "createPin", {
-			...display,
-			fname: user.fname,
-			creamzoId: user.creamzoId,
-		}).then((data) => {
-			refetch();
-			enqueueSnackbar("Pins Created", { variant: "success" });
-		});
-		// console.log(display.tags.split(","))
-	};
+  const handledisplay = () => {
+    if (dis === "none") {
+      setdis("block");
+    } else {
+      setdis("none");
+    }
+  };
+  const handleSubmit = (e) => {
+    console.log("done");
+    axiosSendRequest(AXIOS_ACTIONS.CREATE_PIN, "createPin", {
+      ...display,
+      fname: user.fname,
+      creamzoId: user.creamzoId,
+    }).then((data) => {
+      refetch();
+      enqueueSnackbar("Pins Created", { variant: "success" });
+      history.push("/");
+    });
+    // console.log(display.tags.split(","))
+  };
 
-	return (
-		<React.Fragment>
-			<div style={{ backgroundColor: "rgb(230, 233, 233)", padding: "20px" }}>
-				<div className={style.BoardCreationContainer}>
-					<div className={style.ContentContainer}>
-						<div className={style.leftContainer}>
-							<div>
-								<div
-									className={style.dotbox}
-									style={{ display: dis, zIndex: "1000" }}
-								>
-									<div>Add</div>
-									<div>Delete</div>
-								</div>
-								<div className={style.dots}>
-									<img src={dots} alt="more" onClick={handledisplay} />
-								</div>
+  return (
+    <React.Fragment>
+      <div style={{ backgroundColor: "rgb(230, 233, 233)", padding: "20px" }}>
+        <div className={style.BoardCreationContainer}>
+          <div className={style.ContentContainer}>
+            <div className={style.leftContainer}>
+              <div>
+                <div
+                  className={style.dotbox}
+                  style={{ display: dis, zIndex: "1000" }}
+                >
+                  <div>Add</div>
+                  <div>Delete</div>
+                </div>
+                <div className={style.dots}>
+                  <img src={dots} alt="more" onClick={handledisplay} />
+                </div>
 
-								<div className={style.picture}>
-									<div
-										style={{
-											backgroundColor: "rgb(230, 233, 233)",
-											border: "2px dashed rgb(206, 203, 203)",
-											borderRadius: "15px",
-											display: "flex",
-											alignItems: "center",
-											height: "95%",
-											width: "95%",
-											padding: "10px",
-										}}
-									>
-										<div style={{ width: "100%", padding: "6px" }}>
-											<label for="img">
-												<img
-													style={{
-														width: "25px",
-														height: "25px",
-														marginBottom: "10px",
-													}}
-													src={upload}
-													alt="upload"
-												/>
-												<p
-													style={{
-														color: "black",
-														fontSize: "15px",
-														maxWidth: "180px",
-													}}
-												>
-													Drag and drop a image or upload a image
-												</p>
-												<div className="fileupload">
-													<form action="" style={{ display: "none" }}>
-														{display.imgPath === "" ? (
-															<input
-																type="file"
-																id="img"
-																name="imgPath"
-																accept="image/*"
-																onChange={handlechange}
-															/>
-														) : (
-															false
-														)}
-													</form>
-												</div>
-											</label>
+                <div className={style.picture}>
+                  <div
+                    style={{
+                      backgroundColor: "rgb(230, 233, 233)",
+                      border: "2px dashed rgb(206, 203, 203)",
+                      borderRadius: "15px",
+                      display: "flex",
+                      alignItems: "center",
+                      height: "95%",
+                      width: "95%",
+                      padding: "10px",
+                    }}
+                  >
+                    <div style={{ width: "100%", padding: "6px" }}>
+                      <label for="img">
+                        <img
+                          style={{
+                            width: "25px",
+                            height: "25px",
+                            marginBottom: "10px",
+                          }}
+                          src={upload}
+                          alt="upload"
+                        />
+                        <p
+                          style={{
+                            color: "black",
+                            fontSize: "15px",
+                            maxWidth: "180px",
+                          }}
+                        >
+                          Drag and drop a image or upload a image
+                        </p>
+                        <div className="fileupload">
+                          <form action="" style={{ display: "none" }}>
+                            {display.imgPath === "" ? (
+                              <input
+                                type="file"
+                                id="img"
+                                name="imgPath"
+                                accept="image/*"
+                                onChange={handlechange}
+                              />
+                            ) : (
+                              false
+                            )}
+                          </form>
+                        </div>
+                      </label>
 
-											<p
-												style={{
-													color: "gray",
-													fontSize: "12px",
-													marginTop: "40px",
-													maxWidth: "250px",
-													marginLeft: "auto",
-													marginRight: "auto",
-												}}
-											>
-												Recommended: <br /> Use .jpg files smaller than 1MB
-											</p>
-										</div>
-									</div>
-								</div>
-							</div>
+                      <p
+                        style={{
+                          color: "gray",
+                          fontSize: "12px",
+                          marginTop: "40px",
+                          maxWidth: "250px",
+                          marginLeft: "auto",
+                          marginRight: "auto",
+                        }}
+                      >
+                        Recommended: <br /> Use .jpg files smaller than 1MB
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
 
-							{display.imgPath && (
-								<img
-									className={style.leftContainer}
-									src={URL.createObjectURL(display.imgPath)}
-									style={{
-										position: "relative",
-										width: "100%",
-										height: "60vh",
-										bottom: "60vh",
-										borderRadius: "20px",
-									}}
-									alt=""
-								/>
-							)}
-						</div>
-						<div className={style.rightContiner}>
-							<form className={style.BoardCreationForm} action="">
-								<div className={style.post}>
-									{/* <select style={{ width: "150px", height: "40px", border: "none", borderRadius: "10px 0px 0px 10px", position: "relative", backgroundColor: "rgb(230, 233, 233)" }} >
+              {display.imgPath && (
+                <img
+                  className={style.leftContainer}
+                  src={URL.createObjectURL(display.imgPath)}
+                  style={{
+                    position: "relative",
+                    width: "100%",
+                    height: "60vh",
+                    bottom: "60vh",
+                    borderRadius: "20px",
+                  }}
+                  alt=""
+                />
+              )}
+            </div>
+            <div className={style.rightContiner}>
+              <form className={style.BoardCreationForm} action="">
+                <div className={style.post}>
+                  {/* <select style={{ width: "150px", height: "40px", border: "none", borderRadius: "10px 0px 0px 10px", position: "relative", backgroundColor: "rgb(230, 233, 233)" }} >
                                         <option value="section1">Section1</option>
                                         <option value="section2">Section2</option>
                                         <option value="section3">Section3</option>
                                         <option value="section4">Section4</option>
                                     </select> */}
-									{/* <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel> */}
-									<Select
-										labelId="demo-simple-select-outlined-label"
-										disableUnderline
-										id="demo-simple-select-outlined"
-										style={{
-											width: "150px",
-											height: "40px",
-											paddingLeft: "7px",
-											scrollBehaviour: "auto",
-											borderRadius: "10px 0px 0px 10px",
-											position: "relative",
-											backgroundColor: "rgb(230, 233, 233)",
-										}}
-									>
-										<MenuItem value="section1">Art</MenuItem>
-										<MenuItem value="section2">Culture</MenuItem>
-										<MenuItem value="section3">DIY And Crafts</MenuItem>
-										<MenuItem value="section4">Design</MenuItem>
-										<MenuItem value="section5">Education</MenuItem>
-										<MenuItem value="section6">Environment</MenuItem>
-										<MenuItem value="section7">Events</MenuItem>
-										<MenuItem value="section8">Fashion</MenuItem>
-										<MenuItem value="section9">Food And Drink</MenuItem>
-										<MenuItem value="section10">Hobbies</MenuItem>
-										<MenuItem value="section11">Home Decor</MenuItem>
-										<MenuItem value="section12">Pets</MenuItem>
-										<MenuItem value="section13">Quotes</MenuItem>
-										<MenuItem value="section14">Technology</MenuItem>
-										<MenuItem value="section15">Travel</MenuItem>
-										<MenuItem value="section16">Vehicle</MenuItem>
-									</Select>
-									<Button
-										// type="submit"
-										name=""
-										id={style.boardSubmit}
-										onClick={handleSubmit}
-									>
-										Post
-									</Button>
-								</div>
+                  {/* <InputLabel id="demo-simple-select-outlined-label">Age</InputLabel> */}
+                  <Select
+                    labelId="demo-simple-select-outlined-label"
+                    disableUnderline
+                    id="demo-simple-select-outlined"
+                    style={{
+                      width: "150px",
+                      height: "40px",
+                      paddingLeft: "7px",
+                      scrollBehaviour: "auto",
+                      borderRadius: "10px 0px 0px 10px",
+                      position: "relative",
+                      backgroundColor: "rgb(230, 233, 233)",
+                    }}
+                    name="category"
+                    onChange={handlechange}
+                    value={display.category}
+                  >
+                    <MenuItem value="Art">Art</MenuItem>
+                    <MenuItem value="Culture">Culture</MenuItem>
+                    <MenuItem value="DIY And Crafts">DIY And Crafts</MenuItem>
+                    <MenuItem value="Design">Design</MenuItem>
+                    <MenuItem value="Education">Education</MenuItem>
+                    <MenuItem value="Environment">Environment</MenuItem>
+                    <MenuItem value="Events">Events</MenuItem>
+                    <MenuItem value="Fashion">Fashion</MenuItem>
+                    <MenuItem value="Food And Drink">Food And Drink</MenuItem>
+                    <MenuItem value="Hobbies">Hobbies</MenuItem>
+                    <MenuItem value="Home Decor">Home Decor</MenuItem>
+                    <MenuItem value="Pets">Pets</MenuItem>
+                    <MenuItem value="Quotes">Quotes</MenuItem>
+                    <MenuItem value="Technology">Technology</MenuItem>
+                    <MenuItem value="Travel">Travel</MenuItem>
+                    <MenuItem value="Vehicle">Vehicle</MenuItem>
+                  </Select>
+                  <Button
+                    // type="submit"
+                    name=""
+                    id={style.boardSubmit}
+                    onClick={handleSubmit}
+                  >
+                    Post
+                  </Button>
+                </div>
 
-								<TextField
-									id="standard-basic"
-									onChange={handlechange}
-									name="title"
-									label="Add post title"
-								/>
-								<TextField
-									id="standard-basic"
-									onChange={handlechange}
-									name="description"
-									label="Add description"
-								/>
-								<TextField
-									multiline
-									rows={5}
-									name="tags"
-									onChange={handlechange}
-									label="Tags ( limit 10 , comma separated)"
-								/>
-								<TextField
-									id="standard-basic"
-									multiline
-									rows={2}
-									name="link"
-									onChange={handlechange}
-									label="Add link of blog / destination"
-								/>
-								{/* <input className={style.BoardCreationFormInput} type="text" name="" id="" placeholder="Add your post title" />
+                <TextField
+                  id="standard-basic"
+                  onChange={handlechange}
+                  name="title"
+                  label="Add post title"
+                />
+                <TextField
+                  id="standard-basic"
+                  onChange={handlechange}
+                  name="description"
+                  label="Add description"
+                />
+                <TextField
+                  multiline
+                  rows={5}
+                  name="tags"
+                  onChange={handlechange}
+                  label="Tags ( limit 10 , comma separated)"
+                />
+                <TextField
+                  id="standard-basic"
+                  multiline
+                  rows={2}
+                  name="link"
+                  onChange={handlechange}
+                  label="Add link of blog / destination"
+                />
+                {/* <input className={style.BoardCreationFormInput} type="text" name="" id="" placeholder="Add your post title" />
                             <textarea style={{ backgroundColor: "white" }} name="" id="" cols="30" rows="8" placeholder="Add your Description"></textarea>
                             <input className={style.BoardCreationFormInput} type="text" name="" id="" placeholder="Tags(limit 10,comma separated)" />
                             <input className={style.BoardCreationFormInput} type="text" name="" id="" placeholder="Add a link of your blog or website/destination link" /> */}
-							</form>
-						</div>
-					</div>
-				</div>
-			</div>
-		</React.Fragment>
-	);
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 
 export default BoardCreation;
--- src/Components/Board/BoardDisplay.jsx
+++ src/Components/Board/BoardDisplay.jsx
@@ -13,128 +13,130 @@ import img from "./assets/eric-christian-king.jpg";
 import { useCurrentPin } from "../../Context/PinsContext";
 
 const BoardDisplay = () => {
-	// eslint-disable-next-line
-	const [pinCurrData, setPinCurrData] = useCurrentPin();
+  // eslint-disable-next-line
+  const [pinCurrData, setPinCurrData] = useCurrentPin();
 
-	return (
-		<React.Fragment>
-			<div style={{ backgroundColor: "rgb(230, 233, 233)", padding: "15px" }}>
-				<div className={style.BoardDisplayContainer}>
-					<div className={style.ContentContainerDisplay}>
-						<div className={style.leftContainerDisplay}>
-							<img
-								style={{ width: "100%", height: "100%", borderRadius: "15px" }}
-								src={pinCurrData.imgUrl}
-								alt="addedImage"
-							/>
-						</div>
-						<div className={style.rightContinerDisplay}>
-							<h3 style={{ color: "black" }}>
-								{" "}
-								Title: <small>{pinCurrData.title}</small>{" "}
-							</h3>
-							<h3 style={{ color: "black" }}>
-								{" "}
-								URL:{" "}
-								<small>
-									<a href={pinCurrData.imgUrl}>{pinCurrData.imgUrl}</a>
-								</small>{" "}
-							</h3>
-							<h3 style={{ color: "black" }}>
-								{" "}
-								Description: <br /> <small>{pinCurrData.desc}</small>
-							</h3>
-							<h3 style={{ color: "black" }}>
-								Author: <small>{pinCurrData.author}</small>{" "}
-							</h3>
-							<div style={{ display: "flex", justifyContent: "flex-start" }}>
-								<img
-									style={{ width: "25px", height: "25px" }}
-									src={heart}
-									alt="liked"
-								/>
-								<h3>{pinCurrData.likes?.length}</h3>
-							</div>
-							<div style={{ display: "flex", justifyContent: "flex-start" }}>
-								<img
-									style={{ width: "25px", height: "25px" }}
-									src={views}
-									alt="views"
-								/>
-								<h3>{pinCurrData.tags?.length}</h3>
-							</div>
-							<div style={{ display: "flex", justifyContent: "flex-start" }}>
-								<img
-									style={{ width: "25px", height: "25px" }}
-									src={download}
-									alt="downloads"
-								/>
-								<h3>{pinCurrData.downloads?.length}</h3>
-							</div>
+  return (
+    <React.Fragment>
+      <div style={{ backgroundColor: "rgb(230, 233, 233)", padding: "15px" }}>
+        <div className={style.BoardDisplayContainer}>
+          <div className={style.ContentContainerDisplay}>
+            <div className={style.leftContainerDisplay}>
+              <img
+                style={{ width: "100%", height: "100%", borderRadius: "15px" }}
+                src={pinCurrData.imgUrl}
+                alt="addedImage"
+              />
+            </div>
+            <div className={style.rightContinerDisplay}>
+              <h3 style={{ color: "black" }}>
+                {" "}
+                Title: <small>{pinCurrData.title}</small>{" "}
+              </h3>
+              <h3 style={{ color: "black" }}>
+                {" "}
+                URL:{" "}
+                <small>
+                  <a href={pinCurrData.imgUrl}>{pinCurrData.imgUrl}</a>
+                </small>{" "}
+              </h3>
+              <h3
+                style={{ color: "black", maxHeight: "20vh", overflow: "auto" }}
+              >
+                {" "}
+                Description: <br /> <small>{pinCurrData.desc}</small>
+              </h3>
+              <h3 style={{ color: "black" }}>
+                Author: <small>{pinCurrData.author}</small>{" "}
+              </h3>
+              <div style={{ display: "flex", justifyContent: "flex-start" }}>
+                <img
+                  style={{ width: "25px", height: "25px" }}
+                  src={heart}
+                  alt="liked"
+                />
+                <h3>{pinCurrData.likes?.length}</h3>
+              </div>
+              <div style={{ display: "flex", justifyContent: "flex-start" }}>
+                <img
+                  style={{ width: "25px", height: "25px" }}
+                  src={views}
+                  alt="views"
+                />
+                <h3>{pinCurrData.tags?.length}</h3>
+              </div>
+              <div style={{ display: "flex", justifyContent: "flex-start" }}>
+                <img
+                  style={{ width: "25px", height: "25px" }}
+                  src={download}
+                  alt="downloads"
+                />
+                <h3>{pinCurrData.downloads?.length}</h3>
+              </div>
 
-							<div style={{ display: "flex", justifyContent: "flex-start" }}>
-								<img
-									style={{ width: "25px", height: "25px" }}
-									src={share}
-									alt="shared"
-								/>
-								<h3>{pinCurrData.shares.length}</h3>
-							</div>
+              <div style={{ display: "flex", justifyContent: "flex-start" }}>
+                <img
+                  style={{ width: "25px", height: "25px" }}
+                  src={share}
+                  alt="shared"
+                />
+                <h3>{pinCurrData.shares.length}</h3>
+              </div>
 
-							<h3>Find Creamzo on</h3>
-							<div
-								style={{ height: "30px", display: "flex", paddingTop: "0px" }}
-							>
-								<a href="###">
-									<img
-										style={{
-											width: "24px",
-											height: "24px",
-											marginLeft: "10px",
-										}}
-										src={facebook}
-										alt="facebook"
-									/>
-								</a>
-								<a href="###">
-									<img
-										style={{
-											width: "24px",
-											height: "24px",
-											marginLeft: "10px",
-										}}
-										src={instagram}
-										alt="instagram"
-									/>
-								</a>
-								<a href="##">
-									<img
-										style={{
-											width: "24px",
-											height: "24px",
-											marginLeft: "10px",
-										}}
-										src={twitter}
-										alt="twitter"
-									/>
-								</a>
-								<a href="##">
-									<img
-										style={{
-											width: "24px",
-											height: "24px",
-											marginLeft: "10px",
-										}}
-										src={linkedin}
-										alt="twitter"
-									/>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</React.Fragment>
-	);
+              <h3>Find Creamzo on</h3>
+              <div
+                style={{ height: "30px", display: "flex", paddingTop: "0px" }}
+              >
+                <a href="###">
+                  <img
+                    style={{
+                      width: "24px",
+                      height: "24px",
+                      marginLeft: "10px",
+                    }}
+                    src={facebook}
+                    alt="facebook"
+                  />
+                </a>
+                <a href="###">
+                  <img
+                    style={{
+                      width: "24px",
+                      height: "24px",
+                      marginLeft: "10px",
+                    }}
+                    src={instagram}
+                    alt="instagram"
+                  />
+                </a>
+                <a href="##">
+                  <img
+                    style={{
+                      width: "24px",
+                      height: "24px",
+                      marginLeft: "10px",
+                    }}
+                    src={twitter}
+                    alt="twitter"
+                  />
+                </a>
+                <a href="##">
+                  <img
+                    style={{
+                      width: "24px",
+                      height: "24px",
+                      marginLeft: "10px",
+                    }}
+                    src={linkedin}
+                    alt="twitter"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 };
 export default BoardDisplay;

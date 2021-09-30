f94b5a88844020e31528d83b6184440325be3d39
Parag Katoch <paragkatoch5@gmail.com>
2021-09-30 20:39:20 +0530

    re-login

--- src/Context/Login.js
+++ src/Context/Login.js
@@ -1,4 +1,4 @@
-import React, { createContext, useReducer,  } from "react";
+import React, { createContext, useEffect, useReducer } from "react";
 import { ArticlesProvider } from "./ArticlesContext";
 import { BlogsProvider } from "./BlogsContext";
 import { PinsProvider } from "./PinsContext";
@@ -7,40 +7,42 @@ export const LoginContext = createContext();
 // export const LoginDataContext = createContext()
 
 export const LOGIN_ACTIONS = {
-  LOGIN: "LOGIN",
-  LOGOUT: "LOGOUT",
+	LOGIN: "LOGIN",
+	LOGOUT: "LOGOUT",
 };
 
 const reducer = (login, action) => {
-  switch (action.type) {
-    case LOGIN_ACTIONS.LOGIN:
-      return true;
-    case LOGIN_ACTIONS.LOGOUT:
-      localStorage.removeItem("creamzoUser");
-      // localStorage.setItem("creamzoUser", "");
-      return false;
-    default:
-      return login;
-  }
+	switch (action.type) {
+		case LOGIN_ACTIONS.LOGIN:
+			return true;
+		case LOGIN_ACTIONS.LOGOUT:
+			localStorage.removeItem("creamzoUser");
+			// localStorage.setItem("creamzoUser", "");
+			return false;
+		default:
+			return login;
+	}
 };
-// const user = JSON.parse(localStorage.getItem("creamzoUser"));
+
 export const Login = ({ children }) => {
-  const [login, setLogin] = useReducer(reducer, false);
-  
-  // useEffect(() => {
-  //   if(typeof(user)!=='undefined'){
-  //     setLogin({type:LOGIN_ACTIONS.login})
-  //   }
-   
-  // }, [])
- 
-  return (
-    <LoginContext.Provider value={[login, setLogin]}>
-      <BlogsProvider>
-        <PinsProvider>
-          <ArticlesProvider>{children}</ArticlesProvider>
-        </PinsProvider>
-      </BlogsProvider>
-    </LoginContext.Provider>
-  );
+	const [login, setLogin] = useReducer(reducer, false);
+	const user = JSON.parse(localStorage.getItem("creamzoUser"));
+
+	useEffect(() => {
+		if (user && typeof user !== "undefined") {
+			setLogin({ type: LOGIN_ACTIONS.LOGIN });
+		}
+
+		// eslint-disable-next-line
+	}, []);
+
+	return (
+		<LoginContext.Provider value={[login, setLogin]}>
+			<BlogsProvider>
+				<PinsProvider>
+					<ArticlesProvider>{children}</ArticlesProvider>
+				</PinsProvider>
+			</BlogsProvider>
+		</LoginContext.Provider>
+	);
 };

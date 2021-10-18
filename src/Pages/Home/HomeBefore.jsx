111b577b45c00d98ecce40e8e8b83763a236d633
aRbhaSun20 <remo78295279@gmail.com>
2021-10-18 20:14:37 +0530

    bugs fixes

--- src/Pages/Home/HomeBefore.jsx
+++ src/Pages/Home/HomeBefore.jsx
@@ -1,29 +1,23 @@
-import React, { useState } from 'react';
+import React from "react";
 
-import ImageContainer from '../../Components/ImageContainer_beforeLogin/ImageContainer';
-import { Button } from '@material-ui/core';
-import './style/style.css';
-import img from '../../Components/About/assets/img/art.jpg';
-import img1 from '../../Components/About/assets/img/diy.jpg';
-import img2 from '../../Components/About/assets/img/dog.jpg';
-import img3 from '../../Components/About/assets/img/quotes.jpg';
+import "./style/style.css";
 
-import './style/slideshow.css';
+import "./style/slideshow.css";
 
 const HomeBefore = () => {
-  return (
-    <div className="container1">
-      <div id="slide_bg"></div>
-      <div className="contentAbove">
-        <p className="tagLine">
-          <span style={{ fontWeight: 'bold' }}>LIFE</span> HAS NO{' '}
-          <span style={{ fontWeight: 'bold' }}>COLOR</span> WITHOUT YOUR{' '}
-          <span style={{ fontWeight: 'bold' }}>IMAGINATION</span> <br />
-          IMAGINE <span style={{ fontWeight: 'bold' }}>LIFE </span> !
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container1">
+			<div id="slide_bg"></div>
+			<div className="contentAbove">
+				<p className="tagLine">
+					<span style={{ fontWeight: "bold" }}>LIFE</span> HAS NO{" "}
+					<span style={{ fontWeight: "bold" }}>COLOR</span> WITHOUT YOUR{" "}
+					<span style={{ fontWeight: "bold" }}>IMAGINATION</span> <br />
+					IMAGINE <span style={{ fontWeight: "bold" }}>LIFE </span> !
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default HomeBefore;
